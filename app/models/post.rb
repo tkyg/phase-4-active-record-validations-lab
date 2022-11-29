bad8e78932aac2a2f6d757ccd86bb0ac500f752f
tkyg <80114433+tkyg@users.noreply.github.com>
2022-11-29 16:11:06 -0500

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
-end
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  validate :clickbait
+
+  CLICKBAIT_TITLES = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+
+  def clickbait
+    if CLICKBAIT_TITLES.none? { |c| c.match title } 
+        errors.add(:title, "must be clickbait")
+      end
+    end
+  end
